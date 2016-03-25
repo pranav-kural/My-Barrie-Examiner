60ee2960402fb7972868767cfaef54533ee0be1d
Pranav Kural <pranavkural042@gmail.com>
2016-03-25 19:35:36 -0400

    Initial commenting and updated common.js for hover animations.

--- Scripts/common.js
+++ Scripts/common.js
@@ -1,30 +1,38 @@
-/**
- * Created by Pranav Kural on 2016-03-25.
- */
-//                                              /\
-//-------------------------------------        //\\
-//-----    Animations ---------               //--\\
-//------------------------------------       //    \\
+/* ----------------------------------------------------------
+ *  -- My Barrie Examiner --
+ *  -- Author - Pranav Kural --
+ *  -- Git: https://github.com/pranav-kural/Newspaper-Website.git
+ *  -- Website: http://pranav-kural.github.io/Newspaper-Website/ */
 
 /*------------- Animation-
 --- On Mouse Hover -----*/
-var repeater;
-var property;
-var setValue;
-var changeBackground;
-var increaseFontSize;
-var ChangeColor;
-var ChangeBorder;
-var ChangeShadow;
+var Selector;
+var Property;
 function onHover(selector, property, setValue) {
+    Selector = selector;
+    Property = property;
     var x = document.querySelectorAll(selector);
     for (var i = 0; i < x.length; i++) {
         switch (property) {
             case "changeBackgroundColor": x[i].style.backgroundColor = setValue; break;
-        }
+            case "changeBackgroundImg": x[i].style.backgroundColor = setValue; break;
+            case "changeBackground": x[i].style.backgroundColor = setValue; break;
+            case "changeFontSize": x[i].style.backgroundColor = setValue; break;
+            case "ChangeColor": x[i].style.backgroundColor = setValue; break;
+            case "changeBorder": x[i].style.backgroundColor = setValue; break;
+            case "ChangeShadow": x[i].style.backgroundColor = setValue; break; }
     }
 }
 function onHoverOut() {
-    document.getElementById("hello").style.backgroundColor = "initial";
-    clearInterval(repeater);
+    var x = document.querySelectorAll(Selector);
+    for (var i = 0; i < x.length; i++) {
+        switch (Property) {
+            case "changeBackgroundColor": x[i].style.backgroundColor = "initial"; break;
+            case "changeBackgroundImg": x[i].style.backgroundColor = "initial"; break;
+            case "changeBackground": x[i].style.backgroundColor = "initial"; break;
+            case "changeFontSize": x[i].style.backgroundColor = "initial"; break;
+            case "ChangeColor": x[i].style.backgroundColor = "initial"; break;
+            case "changeBorder": x[i].style.backgroundColor = "initial"; break;
+            case "ChangeShadow": x[i].style.backgroundColor = "initial"; break; }
+    }
 }
